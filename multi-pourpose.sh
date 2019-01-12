dc46584d16b443430095edd47a8b0c0c32e4c2fa
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-01-12 18:44:36 +0000

    added zsh getgist

--- multi-pourpose.sh
+++ multi-pourpose.sh
@@ -1,6 +1,15 @@
 #multi pourpose programs
-sudo apt install -y curl git vim ranger
+sudo apt install -y curl git vim ranger zsh
 
 #node
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt install -y nodejs
+
+#getgists
+sudo apt install python-pip
+export PATH="/home/$USER/.local/bin:$PATH"
+pip install getgist
+export GETGIST_USER=elpopisencio
+
+#oh my zshell
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
