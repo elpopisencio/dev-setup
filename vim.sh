7187c823e95489e8e5023d1fada28bb8315cc0cf
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-01-12 19:39:26 +0000

    added youcompleteme

--- vim.sh
+++ vim.sh
@@ -1,4 +1,4 @@
-sudo apt install vim build-essential cmake python3-dev
+sudo apt install vim
 mkdir -p ~/.vim/pack/bundle/start
 #sudo rm -r ~/.vim/pack/bundle/start/*
 git clone https://github.com/pangloss/vim-javascript ~/.vim/pack/bundle/start/vim-javascript
@@ -6,7 +6,10 @@ git clone https://github.com/elzr/vim-json ~/.vim/pack/bundle/start/vim-json
 git clone https://github.com/mxw/vim-jsx ~/.vim/pack/bundle/start/vim-jsx
 git clone https://github.com/mattn/emmet-vim ~/.vim/pack/bundle/start/emmet-vim
 git clone https://github.com/w0rp/ale ~/.vim/pack/bundle/start/ale
-git clone https://github.com/Valloric/YouCompleteMe ~/.vim/pack/bundle/start/YouCompleteMe 
-python3 ~/.vim/pack/bundle/start/YouCompleteMe/install.py --all
 mkdir ~/.vim/ftplugin
 echo "let b:ale_fixers = ['prettier', 'eslint']" >  ~/.vim/ftplugin/javascript.vim
+
+#you complete me
+sudo apt install vim-youcompleteme
+vam install youcompleteme
+
