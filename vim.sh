e813e295e8da94038ef1de93c2cb094f9a9eb3db
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-07-11 23:16:08 -0300

    added prittier

--- vim.sh
+++ vim.sh
@@ -1,4 +1,4 @@
-sudo apt install vim
+sudo apt install vim-gtk
 mkdir -p ~/.vim/pack/bundle/start
 #sudo rm -r ~/.vim/pack/bundle/start/*
 git clone https://github.com/pangloss/vim-javascript ~/.vim/pack/bundle/start/vim-javascript
@@ -6,6 +6,7 @@ git clone https://github.com/elzr/vim-json ~/.vim/pack/bundle/start/vim-json
 git clone https://github.com/mxw/vim-jsx ~/.vim/pack/bundle/start/vim-jsx
 git clone https://github.com/mattn/emmet-vim ~/.vim/pack/bundle/start/emmet-vim
 git clone https://github.com/w0rp/ale ~/.vim/pack/bundle/start/ale
+git clone https://github.com/prettier/vim-prettier ~/.vim/pack/bundle/start/vim-prettier
 mkdir ~/.vim/ftplugin
 echo "let b:ale_fixers = ['prettier', 'eslint']" >  ~/.vim/ftplugin/javascript.vim
 
